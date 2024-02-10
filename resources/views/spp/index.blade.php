361764bb1a354ad601d8958cb2f7b2fe42bf1056
ra <rahmamangundap@gmail.com>
2024-02-10 19:19:13 +0700

    Tugas CRUD table spps

--- resources/views/spp/index.blade.php
+++ resources/views/spp/index.blade.php
@@ -1,9 +1,9 @@
 @extends('template.master')
 
 @push('css')
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('sbadmin2/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('sbadmin2/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('sbadmin2/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endpush
 
 @section('title', 'Data SPP')
@@ -74,18 +74,18 @@
 @endsection
 
 @push('js')
-  <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/jszip/jszip.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/pdfmake/pdfmake.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/pdfmake/vfs_fonts.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/jszip/jszip.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/pdfmake/pdfmake.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/pdfmake/vfs_fonts.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+  <script src="{{ asset('sbadmin2/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
 
   <script>
     $(function () {
